bc9048a3e0956ff420004dae85c3e86b5ff5e7d6
suli <suli@ulearning.cn>
2017-11-21 17:50:41 +0800

    源码分析

---
+++ analyze.js
@@ -0,0 +1,31 @@
+(function() {
+
+  // Baseline setup
+  // --------------
+  
+  //self 指窗口本身，对窗口自身的只读引用,它返回的对象跟window对象是一模一样的。
+  //在Node.js中全局对象表示global对象
+  
+  	var root = typeof self == 'object' && self.self === self && self ||
+            	typeof global == 'object' && global.global === global && global ||
+            	this ||  {};
+
+            var previousUnderscore = root._;
+
+           // 缓存原型对象
+           //Array.prototype: Array 构造函数的原型
+           //Object.prototype: Object 的原型对象
+           //ES6 新的原始数据类型Symbol，表示独一无二的值
+  	var ArrayProto = Array.prototype, ObjProto = Object.prototype;
+  	var SymbolProto = typeof Symbol !== 'undefined' ? Symbol.prototype : null;
+
+  	// 缓存原型对象上的一些方法
+  	//hasOwnProperty() 方法会返回一个布尔值，
+  	//指示对象是否具有指定的属性作为自身（不继承）属性。
+  	//参数是字符串 名称或者 Symbol。
+  	var push = ArrayProto.push,
+      	slice = ArrayProto.slice,
+      	toString = ObjProto.toString,
+      	hasOwnProperty = ObjProto.hasOwnProperty;
+          
+  }());
